e4f82ee0682148ee36ce7f77825bfcd05f9a5d4d
DenisValuev <valuev.denis91@mail.ru>
2024-08-02 16:23:20 +0300

    Добавьте файлы проекта.

---
+++ Module_3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Module_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            var age = int.Parse(Console.ReadLine());
+            Console.Write("Enter your birthday: ");
+            var birthday = DateOnly.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your name is {name} and age is {age}");
+            Console.WriteLine("Your birthday is {0}", birthday);
+        }
+    }
+}
